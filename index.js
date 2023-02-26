247c3a9176ae5712b405d158aa5b548ed18de721
georgeboholteanu <george.boholteanu@gmail.com>
2023-02-26 20:04:34 +0000

    added WIP inquirer functionality

--- index.js
+++ index.js
@@ -10,5 +10,121 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+// write file with gathered information about team members
+const writeToFile = team => {
+    fs.writeFile(outputPath, render(team), (err) =>
+    err? console.log(err) : console.log("Success!\n")
+    );
+};
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// gather information about the development team members, and render the HTML file.
+const init = () => {
+    let fullResponse = []; 
+
+    inquirer
+    .prompt([
+        {            
+            type: 'input',
+            message: "Team manager name",
+            name: 'managerName',
+            suggestions: ['Oliver', 'Luna', 'Charlie', 'Bella', 'Max', 'Lucy'],            
+        },
+        {            
+            type: 'input',
+            message: "Team manager ID",
+            name: 'managerId'
+        },
+        {            
+            type: 'input',
+            message: "Team manager email",
+            name: 'managerEmail'
+        },
+        {            
+            type: 'input',
+            message: "Team manager office number",
+            name: 'managerNo'
+        },
+        {            
+            type: 'list',
+            message: "Want tot add someone else?",
+            name: 'addTeamMember',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add anyone'],
+        },       
+        
+    ])
+    .then((answers) => {       
+        if (answers.addTeamMember === 'Engineer') {
+            inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: "Engineer name",
+                    name: 'engineerName'
+                },
+                {
+                    type: 'input',
+                    message: "Engineer ID",
+                    name: 'engineerId'
+                },
+                {
+                    type: 'input',
+                    message: "Engineer email",
+                    name: 'engineerEmail'
+                },
+                {
+                    type: 'input',
+                    message: "Engineer github username",
+                    name: 'engineerGithub'
+                }
+            ])
+            .then((answers2) => {
+                fullResponse.push(new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerNo))
+                fullResponse.push(new Engineer(answers2.engineerName, answers2.engineerId, answers2.engineerEmail, answers2.engineerGithub));
+                // console.log(fullResponse);
+            })          
+        } else if (answers.addTeamMember === 'Intern') {
+            inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: "Intern name",
+                    name: 'internName'
+                },
+                {
+                    type: 'input',
+                    message: "Intern ID",
+                    name: 'internId'
+                },
+                {
+                    type: 'input',
+                    message: "Intern email",
+                    name: 'internEmail'
+                },
+                {
+                    type: 'input',
+                    message: "Intern school",
+                    name: 'internSchool'
+                }
+            ])
+            .then((answers2) => {
+                fullResponse.push(new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerNo))
+                fullResponse.push(new Intern(answers2.internName, answers2.internId, answers2.internSchool));
+                // console.log(fullResponse);
+            })
+
+        } else if (answers.addTeamMember === 'I don\'t want to add anyone') {
+            console.log("I don't want to add anyone");
+        }      
+        
+    
+    })
+    .then(() => {
+        // writeToFile(fullResponse)
+        console.log(fullResponse);
+    })
+    .catch((err) => console.error(err));
+}
+
+// function call to initialize program
+init();
+
--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,8 @@
+const Employee = require("../lib/Employee");
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+
 // creates the team
 const generateTeam = team => {
 
