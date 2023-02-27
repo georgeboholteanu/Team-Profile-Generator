85b5741c1cf3bb35dfc6f47205a3af25178e115d
georgeboholteanu <george.boholteanu@gmail.com>
2023-02-27 20:56:39 +0000

    initialize app fn

--- index.js
+++ index.js
@@ -20,45 +20,52 @@ const writeToFile = team => {
 // render the questions for the user
 async function askQuestions() {
     const categoryChoices = ['Manager', 'Engineer', 'Intern'];
-  
+
     const { category } = await inquirer.prompt({
-      type: 'list',
-      name: 'category',
-      message: 'What type of employee do you want to add?',
-      choices: categoryChoices,
+        type: 'list',
+        name: 'category',
+        message: 'What type of employee do you want to add?',
+        choices: categoryChoices,
     });
-  
+
     let employee;
     switch (category) {
-      case 'Manager':
+        case 'Manager':
         employee = new Manager();
         await employee.getDetails();
         break;
-      case 'Engineer':
+        case 'Engineer':
         employee = new Engineer();
         await employee.getDetails();
         break;
-      case 'Intern':
+        case 'Intern':
         employee = new Intern();
         await employee.getDetails();
         break;
-      default:
+        default:
         console.error(`Invalid category "${category}"`);
         return;
     }
-  
+
     const employees = [employee];
-  
+
     const { addAnother } = await inquirer.prompt({
-      type: 'confirm',
-      name: 'addAnother',
-      message: 'Add another employee?',
+        type: 'confirm',
+        name: 'addAnother',
+        message: 'Add another employee?',
     });
-  
+
     if (addAnother) {
-      employees.push(...(await askQuestions()));
+        employees.push(...(await askQuestions()));
     }
-  
+
     return employees;
-  }
-  
+}
+
+// initialize the application
+async function main() {
+    const employees = await askQuestions();
+    console.log(employees);
+}
+
+main();
