23ce93b7de50d013a782112b0c4fbcdd61d5264d
georgeboholteanu <george.boholteanu@gmail.com>
2023-02-28 01:59:06 +0000

    added styling in css and html

--- src/page-template.js
+++ src/page-template.js
@@ -19,57 +19,106 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${capitalizeFirstLetter(manager.name)}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${capitalizeFirstLetter(manager.role)}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
-                <li class="list-group-item">Office number: ${manager.officeNumber}</li>
-            </ul>
-        </div>
-    </div>
+            <div class="card border-dark bg-light m-2">
+                <div class="card-body ">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <img src="" alt="" class="rounded-circle p-1" width="110">
+                        <div class="mt-3">
+                            <h4>${capitalizeFirstLetter(manager.name)}</h4>
+                            <p class="text-secondary mb-1">${capitalizeFirstLetter(manager.role)}</p>
+                            <p class="text-muted font-size-sm">ID: ${manager.id}</p>                                
+                            <a class="btn btn-outline-primary" href="mailto:${manager.email}" role="btn">Message</a>
+                        </div>
+                    </div>
+
+                    <ul class="list-group list-group-flush my-4">
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0">
+                                <i class="fa-regular fa-envelope fa-lg"></i>
+                            </h6>
+                            <span class="text-secondary mx-2"><a href="mailto:${manager.email}">${manager.email}</a></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0">
+                                <i class="fa-solid fa-phone fa-lg"></i>
+                            </h6>
+                            <span class="text-secondary mx-2">${manager.officeNumber}</span>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
         `;
     };
 
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${capitalizeFirstLetter(engineer.name)}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.role}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${capitalizeFirstLetter(engineer.role)}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.mail}">${engineer.email}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.githubUsername}" target="_blank" rel="noopener noreferrer">${engineer.githubUsername}</a></li>
-        </ul>
-    </div>
-</div>
+            <div class="card border-dark bg-light m-2">
+                <div class="card-body ">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <img src="" alt="" class="rounded-circle p-1" width="110">
+                        <div class="mt-3">
+                            <h4>${capitalizeFirstLetter(engineer.name)}</h4>
+                            <p class="text-secondary mb-1">${capitalizeFirstLetter(engineer.role)}</p>
+                            <p class="text-muted font-size-sm">ID: ${engineer.id}</p>                                
+                            <a class="btn btn-outline-primary" href="mailto:${engineer.email}" role="btn">Message</a>
+                        </div>
+                    </div>
+
+                    <ul class="list-group list-group-flush my-4">
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0">
+                                <i class="fa-regular fa-envelope fa-lg"></i>
+                            </h6>
+                            <span class="text-secondary mx-2"><a href="mailto:${engineer.mail}">${engineer.email}</a></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0">
+                                <i class="fa-brands fa-github fa-lg"></i>
+                            </h6>
+                            <span class="text-secondary mx-2"><a href="https://github.com/${engineer.githubUsername}" target="_blank" rel="noopener noreferrer">${engineer.githubUsername}</a></span>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        
         `;
     };
 
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${capitalizeFirstLetter(intern.name)}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${capitalizeFirstLetter(intern.role)}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
-            <li class="list-group-item">School: ${capitalizeFirstLetter(intern.school)}</li>
-        </ul>
-    </div>
-</div>
+            <div class="card border-dark bg-light m-2">
+                <div class="card-body ">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <img src="" alt="" class="rounded-circle p-1" width="110">
+                        <div class="mt-3">
+                            <h4>${capitalizeFirstLetter(intern.name)}</h4>
+                            <p class="text-secondary mb-1">${capitalizeFirstLetter(intern.role)}</p>
+                            <p class="text-muted font-size-sm">ID: ${intern.id}</p>                                
+                            <a class="btn btn-outline-primary" href="mailto:${intern.email}" role="btn">Message</a>
+                        </div>
+                    </div>
+
+                    <ul class="list-group list-group-flush my-4">
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0">
+                                <i class="fa-regular fa-envelope fa-lg"></i>
+                            </h6>
+                            <span class="text-secondary mx-2"><a href="mailto:${intern.mail}">${intern.email}</a></span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="mb-0">
+                                <i class="fa-solid fa-school fa-lg"></i>
+                            </h6>
+                            <span class="text-secondary mx-2">${intern.school}</span>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
         `;
     };
 
@@ -109,7 +158,10 @@ module.exports = team => {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+
+    <!-- ICONS CSS-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -123,10 +175,15 @@ module.exports = team => {
     <div class="container">
         <div class="row">
             <div class="team-area col-12 d-flex justify-content-center">
+                <!-- insert data start -->
                 ${generateTeam(team)}
+                <!-- insert data end -->
             </div>
         </div>
     </div>
+
+    <!-- JS -->
+    <script src="https://kit.fontawesome.com/4a2812a7e8.js" crossorigin="anonymous"></script>
 </body>
 </html>
     `;
